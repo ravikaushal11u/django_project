caf656af401f4208cafb32a0bd38ce96f6b824a9
ravikaushal <inocentboy7284@gmail.com>
2026-08-13 21:22:09 +0530

    Fix duplicate path entries in urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('',views.home, name='home'),
     
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('',views.home, name='home'),
+    
+]
